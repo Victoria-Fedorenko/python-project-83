7b7bd4a8de39ebb2ba36c527c66c9c991de206b7
Victoria-Fedorenko <vnfedorenko97@gmail.com>
2026-03-21 21:47:02 +0300

    fixed linter errors

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,17 +1,16 @@
 from flask import (
     Flask,
-    flash,
-    redirect,
     render_template,
-    request,
-    url_for,
 )
 
 app = Flask(__name__)
 
 app.route('/')
+
+
 def say_hi():
     return render_template('index.html')
+
 
 if __name__ == '__main__':
     app.run(
