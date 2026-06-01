ba8fd218c5d48eb7e91a84df9e421a1332c1b67a
Victoria-Fedorenko <vnfedorenko97@gmail.com>
2026-06-01 22:37:32 +0300

    added action="/urls" to index form

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -67,7 +67,7 @@ def show_url_info(id):
 	return render_template('url_info.html', info=info, result=result)
 
 
-@app.route('/urls', methods=["GET", "POST"])
+@app.route('/urls')
 def show_all_urls():
 	urls = repo.get_urls()
 	return render_template('all_urls.html', urls=urls)
